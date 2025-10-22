0f1ca1080dd93bc0d77cc3c64088cbc08931082d
wirdaini <wirda24si@mahasiswa.pcr.ac.id>
2025-10-23 04:52:39 +0700

    perubahan3

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\FormPengaduanController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SuratController;
+use App\Http\Controllers\WargaController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\FormPengaduanController;
 
 
 Route::get('/', function () {
@@ -17,3 +19,7 @@ Route::get('/auth', [AuthController::class, 'index'])->name('auth.index');
 Route::post('/auth/login', [AuthController::class, 'login'])->name('auth.login');
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::resource('warga', WargaController::class);
+
+Route::resource('surat', SuratController::class);
