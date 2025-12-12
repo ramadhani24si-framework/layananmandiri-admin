1017724b61a2272e3d7f203664db2326822ca871
faqihsib <faqih24si@mahasiswa.pcr.ac.id>
2025-12-12 08:18:09 +0700

    update

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -12,7 +12,11 @@
                 @endauth
                 @guest
                     <a href="{{ route('login') }}" class="btn btn-outline-light btn-sm">Login</a>
+
                     <a href="{{ route('register') }}" class="btn btn-outline-light btn-sm">Logout</a>
+
+                    <a href="{{ route('register') }}" class="btn btn-outline-light btn-sm">Register</a>
+
                 @endguest
             </div>
         </div>
